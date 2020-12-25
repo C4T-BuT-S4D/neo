37244cdb3c8f2ee85d943ec499eed955a2661661
jnovikov <johnnovikov0@gmail.com>
2020-12-25 20:59:22 +0300

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,6 +4,7 @@ mkdir -p neo_client
 cp configs/client/config.yml neo_client/config.yml
 mkdir -p neo_client/exploits
 touch neo_client/exploits/.keep
+cp -r client_env/ neo_client/
 
 mkdir -p neo_server/data
 cp configs/server/config.yml neo_server/config.yml
